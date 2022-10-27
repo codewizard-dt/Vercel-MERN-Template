0e7b899af814238210b942073bd6005a1121992f
David Taylor <94571813+codewizard-dt@users.noreply.github.com>
2022-10-27 16:52:36 -0500

    serverless function for server/index.js

--- deploy.sh
+++ deploy.sh
@@ -3,26 +3,26 @@
 echo ""
 echo "Vercel deployment script"
 # create /dist folder if does not exist
-# if [ -d "dist" ]; then
-#   rm -r dist/*
-# else
-#   mkdir dist && echo " - /dist created" 
-# fi
+if [ -d "dist" ]; then
+  rm -r dist/*
+else
+  mkdir dist && echo " - /dist created" 
+fi
 
-# echo ""
-# echo "React build step"
+echo ""
+echo "React build step"
 
-# cp -r ./client/build/* ./dist && echo " - /client/build/* moved to /dist"
+cp -r ./client/build/* ./dist && echo " - /client/build/* moved to /dist"
 
 echo ""
 echo "Express build step"
-if [ -d "api" ]; then
-  rm -r api/*
+if [ -d "dist/api" ]; then
+  rm -r dist/api/*
 else
-  mkdir api && echo " - /api created for vercel express deployment"
+  mkdir dist/api && echo " - /dist/api created for vercel express deployment"
 fi
 
-cp -r ./server/build/* ./api && echo " - /server/build/* moved to /api"
+cp -r ./server/build/* ./dist/api && echo " - /server/build/* moved to /dist/api"
 
 echo ""
 echo " > Build completed"

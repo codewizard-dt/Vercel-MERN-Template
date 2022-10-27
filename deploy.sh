31453beadd82bfc773bafcd8e30ccdee39e0ac1c
David Taylor <94571813+codewizard-dt@users.noreply.github.com>
2022-10-27 16:38:08 -0500

    build express to /api instead of /dist/api

--- deploy.sh
+++ deploy.sh
@@ -3,26 +3,26 @@
 echo ""
 echo "Vercel deployment script"
 # create /dist folder if does not exist
-if [ -d "dist" ]; then
-  rm -r dist/*
-else
-  mkdir dist && echo " - /dist created" 
-fi
+# if [ -d "dist" ]; then
+#   rm -r dist/*
+# else
+#   mkdir dist && echo " - /dist created" 
+# fi
 
-echo ""
-echo "React build step"
+# echo ""
+# echo "React build step"
 
-mv ./client/build/* ./dist && echo " - /client/build/* moved to /dist"
+# cp -r ./client/build/* ./dist && echo " - /client/build/* moved to /dist"
 
 echo ""
 echo "Express build step"
-if [ -d "dist/api" ]; then
-  rm -r dist/api/*
+if [ -d "api" ]; then
+  rm -r api/*
 else
-  mkdir dist/api && echo " - /dist/api created for vercel express deployment"
+  mkdir api && echo " - /api created for vercel express deployment"
 fi
 
-mv ./server/build/* ./dist/api && echo " - /server/build/* moved to /dist/api"
+cp -r ./server/build/* ./api && echo " - /server/build/* moved to /api"
 
 echo ""
 echo " > Build completed"
